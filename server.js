b8a154f99b69a2812e42e177da255fa771151509
wesley <wesley.sora11@gmail.com>
2022-03-12 22:55:34 +0700

    initial commit

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+const bodyParser = require("body-parser");
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+mongoose.connect(process.env["mongo_key"], {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+const urlSchema = new Schema({
+  original_url: String,
+  short_url: Number,
+});
 
+let Url = mongoose.model("Url", urlSchema);
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
