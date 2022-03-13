eeb8291f46f6bf416233ed13d4e9aab80db37841
wesley08 <43004728+wesley08@users.noreply.github.com>
2022-03-13 15:46:36 +0700

    refactoring

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -4,39 +4,44 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   const getAllUser = await User.find();
-  res.send(getAllUser);
+  res.json(getAllUser);
 });
 
 router.post("/", async (req, res) => {
-  const newUser = await new User({
+  const { username, _id } = await new User({
     username: req.body.username,
   }).save();
 
-  res.send(newUser);
+  res.json({ username, _id });
 });
 
 router.post("/:id/exercises", async (req, res) => {
   let { description, duration, date } = req.body;
-  
-  date = date ?  new Date(date).toDateString() : new Date().toDateString();
+
+  date = date ? new Date(date).toDateString() : new Date().toDateString();
 
   const exercise = {
-    description ,
+    description,
     duration,
-    date
-  }
-  
-  await User.findByIdAndUpdate(req.params.id,{$push: {exercise} }, 
+    date,
+  };
+
+  await User.findByIdAndUpdate(
+    req.params.id,
+    { $push: { exercise } },
     function (err, updatedUser) {
-      if(err) {
-        return console.log('update error:',err);
+      if (err) {
+        return console.log("update error:", err);
       }
       res.json({
-        _id:updatedUser._id,
-        username:updatedUser.username,
-        exercise
+        _id: updatedUser._id,
+        username: updatedUser.username,
+        description,
+        duration,
+        date,
       });
-    });
+    }
+  );
 });
 
 router.get("/:id/logs", async (req, res) => {
@@ -44,26 +49,25 @@ router.get("/:id/logs", async (req, res) => {
   from = new Date(from).toDateString();
   to = new Date(to).toDateString();
 
-    const conditions = {
-    _id:req.params.id,
+  const conditions = {
+    _id: req.params.id,
     ...(from === true &&
       to === true && {
         "exercise.date": {
-            $gt: from,
-            $lt: to,
-          },
-        })
-    };
+          $gt: from,
+          $lt: to,
+        },
+      }),
+  };
 
-  const {_id,exercise,username} = await User.findOne(conditions).exec();
+  const { _id, exercise, username } = await User.findOne(conditions).exec();
 
   res.json({
     _id,
     exercise,
     username,
-    count:exercise.length
-    }
-  );
+    count: exercise.length,
+  });
 });
 
 module.exports = router;
